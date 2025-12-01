fda03b2acf7db1b1d524b240d790cb166535c466
sd7n <SDedicatoria8@gmail.com>
2025-12-01 21:30:53 +0000

    Add AuthPage placeholder

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Authentication'),
+      ),
+      body: const Center(
+        child: Text('Auth Page'),
+      ),
+    );
+  }
+}

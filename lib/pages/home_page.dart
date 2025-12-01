4a56c24bb57134bd962d080330bb427164a30662
sd7n <SDedicatoria8@gmail.com>
2025-12-01 21:20:22 +0000

    Update ProductPage imports

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:union_shop/product_page.dart';
+import 'package:union_shop/pages/product_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});

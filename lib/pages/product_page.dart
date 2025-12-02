d048d7b202fbcb435ec7254d196fbf0453b28458
sd7n <SDedicatoria8@gmail.com>
2025-12-02 02:30:01 +0000

    Add product image placeholder to ProductPage

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -13,7 +13,19 @@ class ProductPage extends StatelessWidget {
     return PageShell(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [],
+        children: [
+          Container(
+            height: 240,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Center(
+              child: Icon(Icons.image, size: 72),
+            ),
+          ),
+          const SizedBox(height: 24),
+        ],
       ),
     );
   }

25bf2edec43668fc39b364d512d1cf1bf9032c29
sd7n <SDedicatoria8@gmail.com>
2025-12-02 03:37:45 +0000

    Create product data model

---
+++ lib/models/product.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+  final bool isLocalImage;
+  final String collectionId;
+  final List<String> sizes;
+  final String? externalUrl;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.isLocalImage,
+    required this.collectionId,
+    required this.sizes,
+    this.externalUrl,
+  });
+}

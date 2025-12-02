2cacf7a25bab01f583bfc4c691c0b7f9ddc275bc
sd7n <SDedicatoria8@gmail.com>
2025-12-02 03:43:15 +0000

    Create CartItem model for cart system

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,25 @@
+import 'product.dart';
+
+class CartItem {
+  final Product product;
+  final int quantity;
+  final String size;
+
+  const CartItem({
+    required this.product,
+    required this.quantity,
+    required this.size,
+  });
+
+  CartItem copyWith({
+    Product? product,
+    int? quantity,
+    String? size,
+  }) {
+    return CartItem(
+      product: product ?? this.product,
+      quantity: quantity ?? this.quantity,
+      size: size ?? this.size,
+    );
+  }
+}

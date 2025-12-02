8825094864393b6f617d0f4fdbb22e7f149c027e
sd7n <SDedicatoria8@gmail.com>
2025-12-02 03:40:29 +0000

    Create Collection data model

---
+++ lib/models/collection.dart
@@ -0,0 +1,15 @@
+class Collection {
+  final String id;
+  final String name;
+  final String description;
+  final String imageUrl;
+  final bool isLocalImage;
+
+  const Collection({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+    required this.isLocalImage,
+  });
+}

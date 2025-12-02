205a2fdc2257a60e3eb349f346cfa6ff9f96c532
sd7n <SDedicatoria8@gmail.com>
2025-12-02 17:54:30 +0000

    Add collections to DataService for managing product categories

--- lib/services/data_service.dart
+++ lib/services/data_service.dart
@@ -1,8 +1,47 @@
 import '../models/app_data.dart';
+import '../models/collection.dart';
 
 class DataService {
   static final DataService instance = DataService._();
   DataService._();
 
   late final AppData appData;
+
+  final _collections = [
+    Collection(
+      id: 'hoodies',
+      name: 'Hoodies',
+      description: 'Official university hoodies',
+      imageUrl: 'assets/images/hoodie_collection.png',
+      isLocalImage: true,
+    ),
+    Collection(
+      id: 'tshirts',
+      name: 'T-Shirts',
+      description: 'Casual tees & campus shirts',
+      imageUrl: 'assets/images/tshirt_collection.png',
+      isLocalImage: true,
+    ),
+    Collection(
+      id: 'accessories',
+      name: 'Accessories',
+      description: 'Mugs, bags, lanyards and more',
+      imageUrl: 'assets/images/accessories_collection.png',
+      isLocalImage: true,
+    ),
+    Collection(
+      id: 'stationery',
+      name: 'Stationery',
+      description: 'Notebooks, pens and study essentials',
+      imageUrl: 'assets/images/stationery_collection.png',
+      isLocalImage: true,
+    ),
+    Collection(
+      id: 'printshack',
+      name: 'Print Shack',
+      description: 'Custom printing & personalised gifts',
+      imageUrl: 'assets/images/printshack_collection.png',
+      isLocalImage: true,
+    ),
+  ];
 }

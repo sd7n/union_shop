e8f2dc5a1810d9818c0e06e733b9b241039031e1
sd7n <SDedicatoria8@gmail.com>
2025-12-03 20:35:50 +0000

    Create ProductTile Widget to replace ProductGridCard

---
+++ lib/widgets/product_tile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductTile extends StatelessWidget {
+  final Product product;
+  final VoidCallback onTap;
+
+  const ProductTile({
+    super.key,
+    required this.product,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(0),
+              child: product.isLocalImage
+                  ? Image.asset(product.imageUrl, fit: BoxFit.cover)
+                  : Image.network(product.imageUrl, fit: BoxFit.cover),
+            ),
+          ),
+          const SizedBox(height: 6),
+          Text(
+            product.name,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+              height: 1.2,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            '£${product.price.toStringAsFixed(2)}',
+            style: const TextStyle(
+              fontSize: 13,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

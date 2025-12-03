5b57daaa7f133dd0005654c10f7e3c9859c08652
sd7n <SDedicatoria8@gmail.com>
2025-12-03 22:00:52 +0000

    Fix footer overlap

--- lib/widgets/page_shell.dart
+++ lib/widgets/page_shell.dart
@@ -5,7 +5,6 @@ import 'navbar_drawer.dart';
 
 class PageShell extends StatelessWidget {
   final Widget child;
-
   final bool fullWidth;
 
   const PageShell({
@@ -20,28 +19,26 @@ class PageShell extends StatelessWidget {
       appBar: const Navbar(),
       endDrawer: const NavbarDrawer(),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: fullWidth
-                    ? child
-                    : Center(
-                        child: ConstrainedBox(
-                          constraints: const BoxConstraints(maxWidth: 1200),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 24,
-                            ),
-                            child: child,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              fullWidth
+                  ? child
+                  : Center(
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 1200),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 24,
                           ),
+                          child: child,
                         ),
                       ),
-              ),
-            ),
-            const Footer(),
-          ],
+                    ),
+              const Footer(),
+            ],
+          ),
         ),
       ),
     );

884b2410433ef2a184a9d029515d20def462aa50
sd7n <SDedicatoria8@gmail.com>
2025-12-03 20:37:04 +0000

    Migrate ProductTile Widget to HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/widgets/page_shell.dart';
+import 'package:union_shop/widgets/product_tile.dart';
 import '../services/data_service.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,23 +10,9 @@ class HomeScreen extends StatelessWidget {
     Navigator.pushNamed(context, '/collections');
   }
 
-  void _goToProduct(BuildContext context, String productId) {
-    Navigator.pushNamed(
-      context,
-      '/product',
-      arguments: productId,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final isWide = MediaQuery.of(context).size.width >= 600;
-    final data = DataService.instance;
-    final featured = [
-      data.getProduct('hoodie-navy'),
-      data.getProduct('tee-classic'),
-      data.getProduct('mug-uni'),
-    ];
 
     return PageShell(
       child: Column(
@@ -124,194 +111,73 @@ class HomeScreen extends StatelessWidget {
               if (constraints.maxWidth < 600) {
                 return Column(
                   children: [
-                    ...featured.map((p) {
-                      return Column(
-                        children: [
-                          _FeaturedProductCard(
-                            title: p.name,
-                            price: '£${p.price.toStringAsFixed(2)}',
-                            onView: () => _goToProduct(context, p.id),
-                          ),
-                          const SizedBox(height: 12),
-                        ],
-                      );
-                    }).toList(),
+                    ProductTile(
+                      product: DataService.instance.getProduct('hoodie-navy')!,
+                      onTap: () => Navigator.pushNamed(
+                        context,
+                        '/product',
+                        arguments: 'hoodie-navy',
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    ProductTile(
+                      product: DataService.instance.getProduct('tee-classic')!,
+                      onTap: () => Navigator.pushNamed(
+                        context,
+                        '/product',
+                        arguments: 'tee-classic',
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    ProductTile(
+                      product: DataService.instance.getProduct('mug-uni')!,
+                      onTap: () => Navigator.pushNamed(
+                        context,
+                        '/product',
+                        arguments: 'mug-uni',
+                      ),
+                    ),
                   ],
                 );
               }
               return GridView.count(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
-                crossAxisCount: 2,
+                crossAxisCount: 3,
                 crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                childAspectRatio: 0.85,
-                children: featured.map((p) {
-                  return _FeaturedProductCard(
-                    title: p.name,
-                    price: '£${p.price.toStringAsFixed(2)}',
-                    onView: () => _goToProduct(context, p.id),
-                  );
-                }).toList(),
+                mainAxisSpacing: 24,
+                childAspectRatio: 0.8,
+                children: [
+                  ProductTile(
+                    product: DataService.instance.getProduct('hoodie-navy')!,
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      '/product',
+                      arguments: 'hoodie-navy',
+                    ),
+                  ),
+                  ProductTile(
+                    product: DataService.instance.getProduct('tee-classic')!,
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      '/product',
+                      arguments: 'tee-classic',
+                    ),
+                  ),
+                  ProductTile(
+                    product: DataService.instance.getProduct('mug-uni')!,
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      '/product',
+                      arguments: 'mug-uni',
+                    ),
+                  ),
+                ],
               );
             },
           ),
           SizedBox(height: isWide ? 48 : 32),
         ],
-      ),
-    );
-  }
-}
-
-class _FeaturedProductCard extends StatelessWidget {
-  final String title;
-  final String price;
-  final VoidCallback onView;
-
-  const _FeaturedProductCard({
-    required this.title,
-    required this.price,
-    required this.onView,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final inGrid = constraints.maxWidth < 400;
-        final imageSize = inGrid ? 60.0 : 72.0;
-        final padding = inGrid ? 8.0 : 12.0;
-
-        return _HoverableProductCard(
-          imageSize: imageSize,
-          padding: padding,
-          title: title,
-          price: price,
-          onView: onView,
-        );
-      },
-    );
-  }
-}
-
-class _HoverableProductCard extends StatefulWidget {
-  final double imageSize;
-  final double padding;
-  final String title;
-  final String price;
-  final VoidCallback onView;
-
-  const _HoverableProductCard({
-    required this.imageSize,
-    required this.padding,
-    required this.title,
-    required this.price,
-    required this.onView,
-  });
-
-  @override
-  State<_HoverableProductCard> createState() => _HoverableProductCardState();
-}
-
-class _HoverableProductCardState extends State<_HoverableProductCard> {
-  bool _isHovered = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      child: TweenAnimationBuilder<double>(
-        tween: Tween(begin: 1.0, end: _isHovered ? 1.02 : 1.0),
-        duration: const Duration(milliseconds: 200),
-        builder: (context, scale, child) {
-          return Transform.scale(
-            scale: scale,
-            child: child,
-          );
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            border: _isHovered
-                ? Border.all(color: Theme.of(context).colorScheme.secondary)
-                : null,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Card(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Padding(
-                padding: EdgeInsets.all(widget.padding),
-                child: Row(
-                  children: [
-                    Container(
-                      width: widget.imageSize,
-                      height: widget.imageSize,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade100,
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                      child: const Center(
-                        child: Icon(
-                          Icons.image,
-                          size: 24,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.title,
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleMedium
-                                ?.copyWith(
-                                  fontWeight: FontWeight.w600,
-                                ),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            widget.price,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium
-                                ?.copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).colorScheme.secondary,
-                                ),
-                          ),
-                          const SizedBox(height: 8),
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: TextButton(
-                              onPressed: widget.onView,
-                              style: TextButton.styleFrom(
-                                padding: EdgeInsets.zero,
-                                foregroundColor:
-                                    Theme.of(context).colorScheme.primary,
-                              ),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: const [
-                                  Text('View'),
-                                  SizedBox(width: 4),
-                                  Icon(Icons.chevron_right, size: 18),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }

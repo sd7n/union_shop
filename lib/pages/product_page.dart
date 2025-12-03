92fc9bf7ae43199b0ddf3597cc17b0b3e2b9d23e
sd7n <SDedicatoria8@gmail.com>
2025-12-03 02:00:23 +0000

    Implement reusable _launchUrl helper to ProductPage

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -18,6 +18,16 @@ class _ProductPageState extends State<ProductPage> {
   String? selectedSize;
   int quantity = 1;
 
+  Future<void> _launchUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(
+      uri,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;

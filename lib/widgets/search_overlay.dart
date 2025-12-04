d8cbff28d0cd487d71238d4385703eb71e32a4ef
sd7n <SDedicatoria8@gmail.com>
2025-12-04 04:04:30 +0000

    Create searchOverlay widget

---
+++ lib/widgets/search_overlay.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchOverlay extends StatelessWidget {
+  const SearchOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

0d97f56d67d0db738a02fee0b42f3f60f24f3a24
sd7n <SDedicatoria8@gmail.com>
2025-12-04 01:02:11 +0000

    Create HeroSlide model

---
+++ lib/models/hero_slide.dart
@@ -0,0 +1,15 @@
+class HeroSlide {
+  final String image;
+  final String title;
+  final String subtitle;
+  final String buttonText;
+  final String buttonRoute;
+
+  const HeroSlide({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    required this.buttonText,
+    required this.buttonRoute,
+  });
+}

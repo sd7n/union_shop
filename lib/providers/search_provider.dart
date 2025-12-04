00f398ecc30d2c9f3fdcc2381ce79ad727d49103
sd7n <SDedicatoria8@gmail.com>
2025-12-04 04:02:46 +0000

    Apply setSearchTerm method

--- lib/providers/search_provider.dart
+++ lib/providers/search_provider.dart
@@ -4,4 +4,9 @@ import '../models/product.dart';
 class SearchProvider extends ChangeNotifier {
   String searchTerm = '';
   List<Product> results = [];
+
+  void setSearchTerm(String term) {
+    searchTerm = term;
+    notifyListeners();
+  }
 }

95bf4543c93533846ab4d6e00a1e9c56f0e2a187
sd7n <SDedicatoria8@gmail.com>
2025-12-04 04:05:11 +0000

    Improve SearchOverlay to include visibility control with AnimatedPositioned

--- lib/widgets/search_overlay.dart
+++ lib/widgets/search_overlay.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 class SearchOverlay extends StatelessWidget {
-  const SearchOverlay({super.key});
+  final bool isVisible;
+
+  const SearchOverlay({
+    super.key,
+    required this.isVisible,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedPositioned(
+      top: isVisible ? 0 : -100,
+      left: 0,
+      right: 0,
+      height: 80,
+      duration: const Duration(milliseconds: 300),
+      child: Container(
+        color: Colors.white,
+        child: const SizedBox.shrink(),
+      ),
+    );
   }
 }
